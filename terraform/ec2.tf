2b314661db0cabbd6de81abc28a66dcc890c3b87
shirkang <gauri.shirkande@roche.com>
2026-07-18 11:04:48 +0530

    Modifying ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "git::https://github.com/gauri17-pro/terraform-modules.git?ref=v1.0.1"
+    source = "git::https://github.com/gauri17-pro/terraform-modules.git//aws/ec2?ref=v1.0.1"
     aws_region = "ap-south-1"
     instance_type = "t2.medium"
     key_pair = "gauri-key-pair"
